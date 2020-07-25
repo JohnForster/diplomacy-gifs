8d2710195a0bc9e6a9c2d0c1cba4a96cbcbafb62
John Forster <johnforster@bulb.co.uk>
2020-07-25 13:24:30 +0100

    Add gif mime type

--- src/utils/setGif.ts
+++ src/utils/setGif.ts
@@ -1,9 +1,9 @@
-import { MagickFile, loadImageElement, MagickInputFile, MagickOutputFile } from 'wasm-imagemagick';
+import { MagickFile, MagickOutputFile } from 'wasm-imagemagick';
 
 const setGif = (imageData: MagickFile, HTMLElement: HTMLImageElement | null): void => {
   if (!HTMLElement) return;
   const img = imageData as MagickOutputFile;
-  const blob = new Blob([img.buffer as ArrayBuffer]);
+  const blob = new Blob([img.buffer as ArrayBuffer], {type: 'image/gif'});
   const objectURL = URL.createObjectURL(blob);
   HTMLElement.src = objectURL;
 };
